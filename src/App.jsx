0c4ce39fb14930627de84fe839ab87a2d1deab57
Rowmel Marco Dabon <rmdabs@gmail.com>
2024-06-14 23:14:38 +0800

    feat: add dark mode support

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import './App.css'
-import { MainPage } from '@/features/main-page'
-import { Toaster } from './components/ui/toaster'
+import "./App.css";
+import { MainPage } from "@/features/main-page";
+import { Toaster } from "./components/ui/toaster";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   return (
-    <>
+    <ThemeProvider>
       <MainPage />
       <Toaster />
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- src/features/main-page/index.jsx
+++ src/features/main-page/index.jsx
@@ -1,11 +1,15 @@
+import { ThemeToggler } from "@/components/theme-toggler";
 import { ExpenseTable } from "./components/expense-table";
 
 export function MainPage() {
   return (
     <main className="flex flex-col items-center justify-center w-full max-w-5xl m-auto p-5 gap-6">
-      <h1 className="font-sans w-full scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        CentTrak
-      </h1>
+      <div className="flex flex-row items-center w-full justify-between">
+        <h1 className="font-sans w-full scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          CentTrak
+        </h1>
+        <ThemeToggler />
+      </div>
       <div className="flex flex-row w-full justify-between items-center">
         <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
           Your Expenses
