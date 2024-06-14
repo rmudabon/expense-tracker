4c4c6192affcb2e0f7980c6d7a815c24c5725ca4
Rowmel Marco Dabon <rmdabs@gmail.com>
2024-06-14 08:53:49 +0800

    feat: initial layout

---
+++ src/features/main-page/index.jsx
@@ -0,0 +1,9 @@
+export function MainPage() {
+    return (
+            <div className='flex flex-col h-full items-center justify-center'>
+                <h1 className="font-sans scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                    Expense Tracker
+                </h1>
+            </div>
+    )
+}
